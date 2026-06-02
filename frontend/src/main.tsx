fbc910881daa6a043d99eced31e257a433fae0b8
ivan <ivanfan@connect.hku.hk>
2026-06-02 22:17:36 +0800

    前端添加 register 以及 login 页面

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import App from './App'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <App />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
     <Analytics />
   </React.StrictMode>,
 )
